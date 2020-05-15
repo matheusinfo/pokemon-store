fd194db1af58a60224a747213b5de50b252cc500
MatheusINFO <matheus.add12@gmail.com>
2020-05-15 11:10:26 -0300

    skeleton

---
+++ src/components/Skeleton/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+    width: 180px;
+
+    div{
+        margin: 20px 0;
+        justify-content: center;
+    }
+`;
+
+export const ImageDiv = styled.div`
+    display: flex;
+    justify-content: center;
+`;
